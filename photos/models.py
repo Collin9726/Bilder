824c2c72522d8983eb0629bf7a64378b6bacf351
collin9726 <owino.collin@gmail.com>
2020-06-27 15:47:55 +0300

    add models and upload photo

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location_name= models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.location_name 
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self):
+        pass
+
+
+
+class Category(models.Model):
+    category_name= models.CharField(max_length =30) 
+
+    def __str__(self):
+        return self.category_name 
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self):
+        pass
+
+
+
+class Image(models.Model):    
+    post_date = models.DateTimeField(auto_now_add=True)
+    image_url = models.ImageField(upload_to = 'posts/')
+    image_name = models.CharField(max_length =30)
+    image_description = models.TextField()
+    image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        pass
+
+    def get_image_by_id(cls,id):
+        pass
+
+    def search_image(cls, category):
+        pass
+
+    def filter_by_location(cls, location):
+        pass
--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Image, Category
 
 # Create your views here.
-def welcome(request):
+def welcome(request):    
+    
     return render(request, 'welcome.html')
+
+def home(request):
+    pics=[]
+    categories=Category.objects.all()
+    for cat in categories:
+        photos=Image.objects.filter(image_category = cat)
+        pics.append(photos)
+    
+    return render(request, 'all.html', {"pics": pics})
+
