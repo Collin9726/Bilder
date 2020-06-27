cf22970e3e145ccd6cd0f1bf2529c3b9df558ff3
collin9726 <owino.collin@gmail.com>
2020-06-27 17:13:54 +0300

    style photos and update navbar to include locations and categories

--- photos/views.py
+++ photos/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
-from .models import Image, Category
+from .models import Image, Category, Location
 
 # Create your views here.
-def welcome(request):    
+def welcome(request):  
+    categories=Category.objects.all()
+    locations=Location.objects.all()  
     
-    return render(request, 'welcome.html')
+    return render(request, 'welcome.html', {"categories": categories, "locations": locations})
 
 def home(request):
     pics=[]
     categories=Category.objects.all()
+    locations=Location.objects.all()
     for cat in categories:
         photos=Image.objects.filter(image_category = cat)
         pics.append(photos)
     
-    return render(request, 'all.html', {"pics": pics})
+    return render(request, 'all.html', {"pics": pics, "categories": categories, "locations": locations})
 
