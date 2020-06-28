0f410911de69c6a1fe01657151306a475e73b443
collin9726 <owino.collin@gmail.com>
2020-06-28 16:16:35 +0300

    run tests for Category model methods

--- photos/tests.py
+++ photos/tests.py
@@ -36,10 +36,50 @@ class LocationTestClass(TestCase):
         location.location_name = 'otherPlace'
         location.update_location()
         query_set = Location.objects.all()[:1]
-        new_location=None
+        updated_location=None
         for place in query_set:
-            new_location = place
-        self.assertEqual(new_location.location_name, 'otherPlace')
+            updated_location = place
+        self.assertEqual(updated_location.location_name, 'otherPlace')
+
+
+
+class CategoryTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.this_category = Category(category_name='Workout')
+
+    def tearDown(self):
+        Category.objects.all().delete()
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.this_category,Category))
+
+    # Testing Save Method
+    def test_save_category_method(self):
+        self.this_category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    # Testing Delete Method
+    def test_delete_category_method(self):
+        self.this_category.save_category()
+        category = Category.objects.get(category_name = 'Workout')
+        category.delete_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) == 0)
+
+    # Testing Update Method
+    def test_update_category_method(self):
+        self.this_category.save_category()
+        category = Category.objects.get(category_name = 'Workout')     
+        category.category_name = 'otherCategory'
+        category.update_category()
+        query_set = Category.objects.all()[:1]
+        updated_category=None
+        for cat in query_set:
+            updated_category = cat
+        self.assertEqual(updated_category.category_name, 'otherCategory')
     
 
 
